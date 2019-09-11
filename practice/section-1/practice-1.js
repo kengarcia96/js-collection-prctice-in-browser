6750d070bc8947bbd595d4cb610b07c16cd7f05e
John Kenneth Garcia <kengarcia.96@gmail.com>
2019-09-11 20:39:14 +0800

    My Answer for Practice 1

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,21 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return 'Implement the practice require, and begin changing code in this row';
+
+  //return 'Implement the practice require, and begin changing code in this row';
+
+  var commons = [];
+  for (let a=0; a < collectionA.length; a++){
+  	for (let b=0; b < collectionB.length; b++){
+
+  		if (collectionA[a] === collectionB[b]){
+  			commons.push(collectionA[a]);
+  		} 	
+  	}
+
+
+  }
+
+  return commons;
+
 }
